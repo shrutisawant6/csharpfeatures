051057bbb5f1863a13e941171c5f9bf17ae40559
shrutisawant6 <140047758+shrutisawant6@users.noreply.github.com>
2024-02-28 16:35:34 +0530

    Add files via upload

---
+++ CSharpImprovements/ClassRecordStruct.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharpImprovements
+{
+    //record Point
+    //{
+    //    public int X { get; init; }
+    //    public int Y { get; init; }
+    //}
+
+    //class Rectangle
+    //{
+    //    public Point TopLeft { get; set; }
+    //    public Point BottomRight { get; set; }
+    //}
+
+    class ClassPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    struct StructPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public record Record1Person(string Name, int Age);
+
+    class RecordWithinClassPerson
+    {
+        public string Name { get; init; }
+        public int Age { get; init; }
+    }
+}
